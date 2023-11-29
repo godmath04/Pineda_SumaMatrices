373577769ea33110c21a2e268ab7c1f27387e91b
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-11-29 15:31:55 -0500

    Se declara variables y solicita dimensiones matriz

--- sumaMatrices.c
+++ sumaMatrices.c
@@ -7,8 +7,22 @@ Imprimir todas las matrices.*/
 
 int main()
 {
-    printf("--- SUMA MATRICES ---");
+    // Declaracion de variables para cada matriz
 
+    int fila1 = 0, columna1 = 0;
+    int fila2 = 0, columna2 = 0;
+
+    printf("--- SUMA MATRICES ---");
+    // Solicitar dimensiones matriz
+    printf("Ingrese el numero de filas para su primera matriz.\n");
+    scanf("%d", &fila1);
+    printf("Ingrese el numero de columnas para su primera matriz.\n");
+    scanf("%d", &columna1);
+    printf("Ingrese el numero de filas para su segunda matriz.\n");
+    scanf("%d", &fila2);
+    printf("Ingrese el numero de columnas para su segunda matriz.\n");
+    scanf("%d", &columna2);
+    
 
     return 0;
 }
