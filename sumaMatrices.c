75b74ec10d468e257b4ffbc0478900cc697fccc0
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-11-29 15:22:22 -0500

    Agrego encabezado y cambiar datos del commit

---
+++ sumaMatrices.c
@@ -0,0 +1,14 @@
+/* Crear un programa en C que permita sumar 2 matrices de la misma dimensión.
+El usuario ingresa las dimensiones de la matriz. Utilice números aleatorios entre 0 y 100 para inicializar las matrices.
+Imprimir todas las matrices.*/
+
+#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+    printf("--- SUMA MATRICES ---");
+
+
+    return 0;
+}
