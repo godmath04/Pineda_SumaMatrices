7277acca8744271cefe69228e759860c88ae6949
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-11-29 16:02:48 -0500

    Se agrego la suma de las matrices

--- sumaMatrices.c
+++ sumaMatrices.c
@@ -55,5 +55,29 @@ int main()
         printf("\n");
     }
 
+    // SUMA DE LA MATRIZ
+
+    // Verificar dimensiones para la suma
+    if (fila1 == fila2 && columna1 == columna2)
+    {
+        int fila3 = fila1;
+        int columna3 = columna1;
+        int matrizSuma[fila3][columna3];
+        printf("La suma de las matrices es: \n");
+        for (int i = 0; i < fila3; i++)
+        {
+            for (int j = 0; j < columna3; j++)
+            {
+                matrizSuma[i][j] = matrizA[i][j] + matrizB[i][j];
+                printf("%4d   |", matrizSuma[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("No se pueden sumar pues sus dimensiones son diferentes. \n");
+    }
+
     return 0;
 }
